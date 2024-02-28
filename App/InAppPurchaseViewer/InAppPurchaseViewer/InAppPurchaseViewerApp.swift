029473713d0d642f74e86f8e18ed4ab95ff40a66
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2024-02-29 03:04:16 +0900

    Add checking for updates menu button

--- App/InAppPurchaseViewer/InAppPurchaseViewer/InAppPurchaseViewerApp.swift
+++ App/InAppPurchaseViewer/InAppPurchaseViewer/InAppPurchaseViewerApp.swift
@@ -7,17 +7,27 @@
 
 import IAPModel
 import IAPView
+import Sparkle
 import SwiftUI
 
 @MainActor
 @main
 struct InAppPurchaseViewerApp: App {
 
+    private let updaterController = SPUStandardUpdaterController(
+        startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+
     private let model = IAPModel()
 
     var body: some Scene {
         WindowGroup {
             ContentView().environment(model)
+        }
+        .commands {
+            CommandGroup(after: .appInfo) {
+                Button("Check for Updates...",
+                       action: updaterController.updater.checkForUpdates)
+            }
         }
         Settings {
             SettingsView().environment(model)
