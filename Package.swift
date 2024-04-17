7cb9791c5947921d638ce89cbd16f8b679db941e
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2024-04-18 01:20:32 +0900

    Disable upcoming feature because code ql is not supported...

--- Package.swift
+++ Package.swift
@@ -3,19 +3,6 @@
 
 import PackageDescription
 
-let swiftSettings: [SwiftSetting] = [
-    .enableUpcomingFeature("BareSlashRegexLiterals"),
-    .enableUpcomingFeature("ConciseMagicFile"),
-    .enableUpcomingFeature("DeprecateApplicationMain"),
-    .enableUpcomingFeature("DisableOutwardActorInference"),
-    .enableUpcomingFeature("ExistentialAny"),
-    .enableUpcomingFeature("ForwardTrailingClosures"),
-    .enableUpcomingFeature("GlobalConcurrency"),
-    .enableUpcomingFeature("ImplicitOpenExistentials"),
-    .enableUpcomingFeature("ImportObjcForwardDeclarations"),
-    .enableUpcomingFeature("IsolatedDefaultValues"),
-]
-
 let package = Package(
     name: "InAppPurchaseViewer",
     defaultLocalization: "ja",
@@ -56,8 +43,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Dependencies", package: "swift-dependencies"),
                 .product(name: "DependenciesMacros", package: "swift-dependencies"),
-            ],
-            swiftSettings: swiftSettings
+            ]
         ),
         .target(
             name: "IAPInterface",
@@ -65,15 +51,13 @@ let package = Package(
                 "IAPCore",
                 // Use Model...
                 .product(name: "AppStoreServerLibrary", package: "app-store-server-library-swift"),
-            ],
-            swiftSettings: swiftSettings
+            ]
         ),
         .target(
             name: "IAPModel",
             dependencies: [
                 "IAPInterface",
-            ],
-            swiftSettings: swiftSettings
+            ]
         ),
         .testTarget(
             name: "IAPModelTests",
@@ -85,7 +69,6 @@ let package = Package(
                 "IAPModel",
                 .product(name: "Sparkle", package: "Sparkle"),
             ],
-            swiftSettings: swiftSettings,
             plugins: [
                 .plugin(name: "LicensesPlugin", package: "LicensesPlugin"),
             ]
