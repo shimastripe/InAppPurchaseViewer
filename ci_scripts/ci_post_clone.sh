65f8cc1325ee06ee2b73b483d09f1dada850374b
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2024-09-29 03:28:38 +0900

    Fallback swift 5 mode

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,3 +6,7 @@ defaults delete com.apple.dt.Xcode IDEDisableAutomaticPackageResolution
 
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+
+# [Workaround]: Fallback swift 5 mode
+sed -i '' 's/\.v6/\.v5/g' /Volumes/workspace/repository/Package.swift
+sed -i '' 's/SWIFT_VERSION = 6.0/SWIFT_VERSION = 5.0/g' /Volumes/workspace/repository/App/InAppPurchaseViewer/InAppPurchaseViewer.xcodeproj/project.pbxproj
