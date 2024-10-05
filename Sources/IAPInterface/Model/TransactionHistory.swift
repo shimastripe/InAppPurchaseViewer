a665862ca33943c1718b670b192b18b221da8cc4
Go Takagi <15936908+shimastripe@users.noreply.github.com>
2024-10-06 08:13:00 +0900

    Remove rebundunt preconcurrency

--- Sources/IAPInterface/Model/TransactionHistory.swift
+++ Sources/IAPInterface/Model/TransactionHistory.swift
@@ -5,7 +5,7 @@
 //  Created by shimastripe on 2024/02/24.
 //
 
-@preconcurrency import AppStoreServerLibrary  // For Model
+import AppStoreServerLibrary  // For Model
 import Foundation
 
 public struct TransactionHistory: Codable, Hashable, Sendable {
